d2d4b999b2564164e4bc3de05fb51599597c9e06
julien himmer <julienhimmer@kgfgfgfgjhkjkljuhuyhythdesxdsxxdfghjvbnjlmp--jjun.local>
2021-02-20 17:24:18 +0100

    set up ORM

---
+++ seeders/20210220155817-seed-log.js
@@ -0,0 +1,71 @@
+'use strict';
+
+const moment = require("moment")
+
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    /**
+     * Add seed commands here.
+     *
+     * Example:
+     * await queryInterface.bulkInsert('People', [{
+     *   name: 'John Doe',
+     *   isBetaMember: false
+     * }], {});
+    */
+
+/*
+   queryInterface.createTable('Logs', {
+    title: DataTypes.STRING,
+    content: DataTypes.TEXT,
+    heart: DataTypes.BOOLEAN,
+    namespace: DataTypes.STRING,
+    tag: DataTypes.STRING,
+
+  });
+*/
+    return queryInterface.bulkInsert('Logs', [
+{
+title: '!foo',
+content: '!foo',
+heart: true,
+namespace: '!foo',
+tag:'!foo',
+createdAt: moment().format('YYYY-MM-DD HH:mm:ss'),
+updatedAt: moment().format('YYYY-MM-DD HH:mm:ss')
+},
+{
+title: 'bar',
+content: 'bar',
+heart: false,
+namespace: 'bar',
+tag:'bar',
+createdAt: moment().format('YYYY-MM-DD HH:mm:ss'),
+updatedAt: moment().format('YYYY-MM-DD HH:mm:ss')
+},
+{
+title: 'nerd',
+content: 'nerd',
+heart: true,
+namespace: 'nerd',
+tag:'nerd',
+createdAt: moment().format('YYYY-MM-DD HH:mm:ss'),
+updatedAt: moment().format('YYYY-MM-DD HH:mm:ss')
+}
+], {});
+
+
+  },
+
+  down: async (queryInterface, Sequelize) => {
+    /**
+     * Add commands to revert seed here.
+     *
+     * Example:
+     * await queryInterface.bulkDelete('People', null, {});
+     */
+
+
+    return queryInterface.bulkDelete('Logs', null, {});
+  }
+};
--- server/index.js
+++ server/index.js
@@ -1,1 +1,2 @@
-console.log("hello")
+
+const port = 6666
