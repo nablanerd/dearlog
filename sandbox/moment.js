a9ed598e134dffdb38244dc422a50991d07e986c
julien himmer <julienhimmer@kgfgfgfgjhkjkljuhuyhythdesxdsxxdfghjvbnjlmp--jjun.local>
2021-02-20 19:41:54 +0100

    api almost finished

--- sandbox/moment.js
+++ sandbox/moment.js
@@ -1,1 +1,7 @@
-console.log(require('moment')().format('YYYY-MM-DD HH:mm:ss'));
+//console.log(require('moment')().format('YYYY-MM-DD HH:mm:ss'));
+
+const moment = require('moment')
+
+const string ="2021-02-20 18:11:59.530 +00:00"
+
+console.log(moment(new Date(string)).format('DD MMMM HH:mm'))
